4394ab0bc9692417ab00937167e677f817f61a64
HirofumiKanda <nandakandahirofumi322@yahoo.co.jp>
2025-08-14 18:20:39 +0900

    地震の震源の深さに応じてアイコン色分け

--- src/utils/onMapLoad.ts
+++ src/utils/onMapLoad.ts
@@ -1,13 +1,18 @@
 import maplibregl from "maplibre-gl";
 
 export async function onMapLoad(map: maplibregl.Map) {
+  const dark_red = await map.loadImage("img/dark_red.png");
+  map.addImage("dark_red", dark_red.data);
   const red = await map.loadImage("img/red.png");
   map.addImage("red", red.data);
   const orange = await map.loadImage("img/orange.png");
   map.addImage("orange", orange.data);
   const yellow = await map.loadImage("img/yellow.png");
   map.addImage("yellow", yellow.data);
-  const dark_red = await map.loadImage("img/dark_red.png");
-  map.addImage("dark_red", dark_red.data);
-
+  const lime_green = await map.loadImage("img/lime_green.png");
+  map.addImage("lime_green", lime_green.data);
+  const green = await map.loadImage("img/green.png");
+  map.addImage("green", green.data);
+  const blue = await map.loadImage("img/blue.png");
+  map.addImage("blue", blue.data);
 }
